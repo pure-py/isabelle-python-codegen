8398eb7dd7949f8b9793541b116eea22f88d0559
Nikolaus Huber <mail@n-huber.eu>
2026-08-24 22:51:12 +0100

    some cosmetic changes

--- examples/Example_unused_arg.thy
+++ examples/Example_unused_arg.thy
@@ -2,8 +2,8 @@ theory Example_unused_arg
     imports Main "Python.Python_Setup"
 begin
 
-fun const_fn :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
-  "const_fn x _ = x + 1"
+fun const_fn :: "nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat" where
+  "const_fn x _ _ = x + 1"
 
 export_code const_fn in Python file_prefix "."
 
